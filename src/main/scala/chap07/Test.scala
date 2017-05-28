36db5efdf61ba939fc6abda380a96359cbbeea4b
stenoritama <shr.nrht.0623@gmail.com>
2017-05-28 18:07:32 +0900

    chap07

---
+++ src/main/scala/chap07/Test.scala
@@ -0,0 +1,23 @@
+package chap07
+
+object Test extends App {
+
+
+  def makeRowSeq(row: Int) = {
+    for (col <- 1 to 10) yield {
+      val prod = (row * col).toString
+      val padding = " " * (4 - prod.length)
+      padding + prod
+    }
+  }
+
+  def makeRow(row: Int) = makeRowSeq(row).mkString
+
+  def multiTable() = {
+    val tableSeq =
+      for (row <- 1 to 10)
+        yield makeRow(row)
+    tableSeq.mkString("\n")
+  }
+
+}
